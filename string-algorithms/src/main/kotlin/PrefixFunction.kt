1f8511bfc219a32ea1df840d15fd9593ec133495
takahirom <takam.dev@gmail.com>
2020-09-05 15:00:57 +0900

    Add PrefixFunction debug code

--- string-algorithms/src/main/kotlin/PrefixFunction.kt
+++ string-algorithms/src/main/kotlin/PrefixFunction.kt
@@ -7,12 +7,18 @@ class PrefixFunction {
         for (i in string.indices) {
             if (i == 0) continue
             val prev = prefixArray[i - 1] ?: 0
+            println("prev = prefixArray[(i = $i) - 1] :$prev")
+            println("string[prev]:" + string[prev])
+            println("string[i = $i]:" + string[i])
             prefixArray[i] = if (string[prev] == string[i]) {
                 prev + 1
             } else {
                 0
             }
+            println(string.toCharArray().joinToString(" "))
+            println(prefixArray.joinToString(" ") { "" + (it ?: "X") })
         }
+        println(string.toCharArray().joinToString(" "))
         println(prefixArray.joinToString(" "))
     }
 }
